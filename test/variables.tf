cb0984204a75f927b98294d385f033965185b7b1
Musbau Adesola <musbauinfo@gmail.com>
2025-06-22 18:30:14 -0500

    Workflow updated

--- test/variables.tf
+++ test/variables.tf
@@ -17,16 +17,19 @@ variable "region" {
 variable "shared_vpc_project_id" {
   description = "Project ID for Shared VPC host"
   type        = string
+  default = "ava-dev-48515"
 }
 
 variable "service_project_1_id" {
   description = "Project ID for Service Project 1"
   type        = string
+  default = "thriftline-dev-48515"
 }
 
 variable "service_project_2_id" {
   description = "Project ID for Service Project 2"
   type        = string
+  default = "thriftline-uat-48515"
 }
 
 variable "shared_vpc_subnets" {
@@ -36,4 +39,16 @@ variable "shared_vpc_subnets" {
     ip_cidr    = string
     region     = string
   }))
+default = [
+    {
+      name    = "subnet-a"
+      ip_cidr = "10.0.1.0/24"
+      region  = "us-central1"
+    },
+    {
+      name    = "subnet-b"
+      ip_cidr = "10.0.2.0/24"
+      region  = "us-central1"
+    }
+  ]
 }
