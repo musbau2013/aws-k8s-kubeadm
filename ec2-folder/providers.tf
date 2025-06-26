a2dc4b85c1ce82a252e2e56f44fe4b8dd00b8a85
Musbau Adesola <musbauinfo@gmail.com>
2025-06-26 10:13:10 -0500

    Modified the workflow

--- ec2-folder/providers.tf
+++ ec2-folder/providers.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
   # Assume Role with OIDC (no static credentials needed)
   assume_role {
-    role_arn     = var.aws_role_arn # e.g., tfc-oidc-role
+    role_arn     = "arn:aws:iam::381491964789:role/TerraformCloud_Oidc"#var.aws_role_arn # e.g., tfc-oidc-role
     session_name = "terraform-cloud-session"
   }
 }
