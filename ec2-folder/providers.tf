41c3933cb9239493a0f3eda81554d4ac1ee05d4c
Musbau Adesola <musbauinfo@gmail.com>
2025-06-27 10:59:06 -0500

    jghfghjknkfdvd

--- ec2-folder/providers.tf
+++ ec2-folder/providers.tf
@@ -13,8 +13,8 @@ provider "aws" {
   region ="us-east-1" #var.region  # Change to your preferred region
 
   # Assume Role with OIDC (no static credentials needed)
-  assume_role {
-    role_arn     = "arn:aws:iam::381491964789:role/tfc_oidc_role" #var.aws_role_arn 
-    session_name = "terraform-cloud-session"
-  }
+  # assume_role {
+  #   role_arn     = "arn:aws:iam::381491964789:role/tfc_oidc_role" #var.aws_role_arn 
+  #   session_name = "terraform-cloud-session"
+  # }
 }
